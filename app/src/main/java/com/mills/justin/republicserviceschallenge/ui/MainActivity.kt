5fd4cc68501897e60ac3cefacb983eceaf1329dc
Justin Mills <justin.lee.mills@gmail.com>
2023-04-21 22:03:19 -0700

    Use the default AppBar instead of a Toolbar in the layout

--- app/src/main/java/com/mills/justin/republicserviceschallenge/ui/MainActivity.kt
+++ app/src/main/java/com/mills/justin/republicserviceschallenge/ui/MainActivity.kt
@@ -14,19 +14,16 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
-
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        appBarConfiguration = AppBarConfiguration(navController.graph)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
@@ -34,5 +31,4 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
-
 }

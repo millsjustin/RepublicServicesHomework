094222a9141dc94f00457f5085d139bc65761ef0
Justin Mills <justin.lee.mills@gmail.com>
2023-04-21 21:26:00 -0700

    Add routes to display business logic

--- app/src/main/java/com/mills/justin/republicserviceschallenge/data/DriverRepoImpl.kt
+++ app/src/main/java/com/mills/justin/republicserviceschallenge/data/DriverRepoImpl.kt
@@ -37,12 +37,36 @@ class DriverRepoImpl @Inject constructor(
         return localDataSource.routes()
             .map { it.map(this::localRouteToRoute) }
             .map { routes ->
-                // TODO complete business logic
-                // If the driver id is the same as the route id then display the route.
-                // If the driver id is divisible by 2 then show the first R type route.
-                // If the driver id is divisible by 5 then display the second C type route.
-                // If the driver id doesn’t meet any of the conditions above, then show the last I type route.
-                routes.filter { it.id == driverId }
+                val driverIdNum = driverId.toInt()
+
+                // business logic
+                //  If the driver id is the same as the route id then display the route.
+                //  If the driver id is divisible by 2 then show the first R type route.
+                //  If the driver id is divisible by 5 then display the second C type route.
+                //  If the driver id doesn’t meet any of the conditions above, then show the last I type route.
+
+                val idsAreTheSame = routes.firstOrNull { it.id == driverId }
+
+                val divisibleBy2 = if (driverIdNum % 2 == 0) {
+                    routes.firstOrNull { it.type == "R" }
+                } else null
+
+                val divisibleBy5 = if (driverIdNum % 5 == 0) {
+                    routes.asSequence()
+                        .filter { it.type == "C" }
+                        .take(2)
+                        .drop(1)
+                        .firstOrNull()
+                } else null
+
+                val conditionsNotMeet = if (idsAreTheSame == null &&
+                    divisibleBy2 == null &&
+                    divisibleBy5 == null
+                ) {
+                    routes.lastOrNull { it.type == "I" }
+                } else null
+
+                listOfNotNull(idsAreTheSame, divisibleBy2, divisibleBy5, conditionsNotMeet)
             }
             .flowOn(Dispatchers.IO)
     }
